a66adbca71023811df35825a57c058c6a97c7435
Joseph Szafarowicz <joeszafarowicz@gmail.com>
2021-03-12 10:58:28 -0500

    Initial Commit

---
+++ WhatsTheWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsTheWeather
+//
+//  Created by Joseph Szafarowicz on 3/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
